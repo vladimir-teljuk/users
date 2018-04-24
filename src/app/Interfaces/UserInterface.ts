b56eb219bf08d16f4d1fda1e2bdfea0a138bb80a
vladimir_teliuk <vladimir.teljuk@gmail.com>
2018-04-25 00:24:30 +0300

    Add UserInterface

---
+++ src/app/Interfaces/UserInterface.ts
@@ -0,0 +1,11 @@
+export interface UserInterface {
+    name: string;
+    gender: string;
+    phone: string;
+    email: string;
+    username: string;
+    registered: string;
+    city: string;
+}
+
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { GetUsersService } from './services/get-users.service';
+import { UserInterface } from './Interfaces/UserInterface';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.sass']
 })
+
 export class AppComponent implements OnInit {
 
-    public usersList: object[];
+    public usersList: Array<UserInterface> = [];
 
     public constructor(private getUsersService: GetUsersService) {
 
@@ -16,8 +18,17 @@ export class AppComponent implements OnInit {
 
     public ngOnInit(): void {
         this.getUsersService.getUsers(15).subscribe(data => {
-            this.usersList = data.results;
-            console.log(data);
+            for (let i = 0; i < data.results.length; i++) {
+                this.usersList[i] = {
+                    name: data.results[i].name.first + ' ' + data.results[i].name.last,
+                    gender: data.results[i].gender,
+                    phone: data.results[i].phone,
+                    email: data.results[i].email,
+                    username: data.results[i].login.username,
+                    registered: data.results[i].registered,
+                    city: data.results[i].location.city
+                };
+            }
         });
     }
 }
