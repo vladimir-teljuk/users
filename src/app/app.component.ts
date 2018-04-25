d60b97b9e3c7b3968568e9c0a990767ff0e1d604
vladimir_teliuk <vladimir.teljuk@gmail.com>
2018-04-26 01:31:10 +0300

    Add sotr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,9 @@ import { UserInterface } from './Interfaces/UserInterface';
 export class AppComponent implements OnInit {
 
     public usersList: Array<UserInterface> = [];
+    public columnNames: string[] = [ 'Name', 'Gender', 'Phone', 'Email', 'Username', 'Registered', 'City'];
+    public isTrueSort: string[] = ['', '', '', '', '', '', ''];
+
 
     public constructor(private getUsersService: GetUsersService) {
 
@@ -30,5 +33,52 @@ export class AppComponent implements OnInit {
                 };
             }
         });
+    }
+
+    public sortColumn(columnName) {
+        const userList = this.usersList;
+        const isTrueSort = this.isTrueSort;
+        const columnNames = this.columnNames;
+
+        function directSort(list, column) {
+            list.sort((a, b) => {
+                if (a[column] > b[column]) {
+                    return 1;
+                }
+                if (a[column] < b[column]) {
+                    return -1;
+                }
+            });
+            return '↑';
+        }
+
+        function reverseSort(list, column) {
+            list.sort((a, b) => {
+                if (a[column] > b[column]) {
+                    return -1;
+                }
+                if (a[column] < b[column]) {
+                    return 1;
+                }
+            });
+            return '↓';
+        }
+
+        function sort() {
+            let markerSort = '';
+            if (userList[0][columnName] > userList[userList.length - 1][columnName]) {
+                markerSort = directSort(userList, columnName);
+            } else {
+                markerSort = reverseSort(userList, columnName);
+            }
+            console.log(markerSort);
+            for (let i = 0; i < isTrueSort.length; i++) {
+                isTrueSort[i] = '';
+                if (columnNames[i].toLowerCase() === columnName) {
+                    isTrueSort[i] = markerSort;
+                }
+            }
+        }
+        sort();
     }
 }
